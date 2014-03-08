add58857edddbaa8150ff372ba076891af85bce8
Tom Schutter <t.schutter@comcast.net>
2014-03-08 08:31:38 -0700

    height.py: added comments, print intro

--- height.py
+++ height.py
@@ -3,6 +3,9 @@
 import random
 import sys
 
+# Create a dict (dictionary) of races.  The value of each race is a
+# dict of sexes.  The value of each sex is a tuple of minimum height
+# and a variant in height.
 HEIGHT_DATA = {
     "human": {
         "male": (50, 8),
@@ -22,18 +25,21 @@ HEIGHT_DATA = {
 }
 
 def get_height(race, sex):
-    height_data = HEIGHT_DATA[race][sex]
-    height_min, height_variant = height_data
+    """Return a random height based upon race and sex."""
+    height_data = HEIGHT_DATA[race][sex]           # Could combine with next line.
+    height_min, height_variant = height_data       # Unpack tuple.
     height = height_min + random.randint(0, height_variant)
     return height
 
 def main():
-    for race in ["human", "dwarf", "halfling"]:
-        for sex in ["male", "female", "sexless"]:
-            for i in range(3):
-                print(
+    print("Let's generate some characters!")
+    for race in ["human", "dwarf", "halfling"]:    # Loop through races.
+        for sex in ["male", "female", "sexless"]:  # Loop through sexes.
+            for i in range(3):                     # Let's do multiple examples.
+                height = get_height(race, sex)     # Calculate height.
+                print(                             # Output.
                     "%s %s #%i height = %i" %
-                    (race, sex, i, get_height(race, sex))
+                    (race, sex, i, height)
                 )
 
 if __name__ == "__main__":
